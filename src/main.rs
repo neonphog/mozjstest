302c2345b6fb6a9f409d55ed4e562b3d32cec77f
neonphog <neonphog@gmail.com>
2018-12-31 16:06:37 -0700

    cleanup and better js demo

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,84 @@
 #[macro_use]
 extern crate mozjs;
 
-use mozjs::rust::{self, Runtime, SIMPLE_GLOBAL_CLASS};
-use mozjs::jsval::*;
-use mozjs::jsapi::*;
+use mozjs::{
+    rust::{
+        *,
+        jsapi_wrapped::*,
+    },
+    jsapi::{
+        Heap,
+        JSObject,
+        JS_NewGlobalObject,
+        OnNewGlobalHookOption,
+        CompartmentOptions,
+        JSAutoCompartment,
+    },
+    jsval:: {
+        UndefinedValue,
+    },
+};
 
-thread_local!(static RT: (Heap<*mut JSObject>, Runtime) = unsafe {
+static JAVASCRIPT: &'static str = r#"
+function *fibonacci() {
+  let cur = 1
+  let nxt = 1
+  for (;;) {
+    yield cur
+    ;[cur, nxt] = [nxt, cur + nxt]
+  }
+}
+
+(() => {
+  const out = ['fibonacci:']
+  const it = fibonacci()
+  for (let i = 0; i < 10; ++i) {
+    out.push(it.next().value)
+  }
+  return out.join(' ')
+})()
+"#;
+
+fn main() {
     let rt = Runtime::new().unwrap();
 
     let glob: Heap<*mut JSObject> = Heap::default();
-    glob.set(JS_NewGlobalObject(
-        rt.cx(),
-        &SIMPLE_GLOBAL_CLASS,
-        ::std::ptr::null_mut(),
-        OnNewGlobalHookOption::DontFireOnNewGlobalHook,
-        &CompartmentOptions::default()
-    ));
+    let glob_handle = unsafe { Handle::from_raw(glob.handle()) };
 
-    let _ac = JSAutoCompartment::new(rt.cx(), glob.get());
+    let _ac = unsafe {
+        glob.set(JS_NewGlobalObject(
+            rt.cx(),
+            &SIMPLE_GLOBAL_CLASS,
+            ::std::ptr::null_mut(),
+            OnNewGlobalHookOption::DontFireOnNewGlobalHook,
+            &CompartmentOptions::default()
+        ));
 
-    JS_InitReflectParse(rt.cx(), glob.handle());
-    JS_FireOnNewGlobalObject(rt.cx(), glob.handle());
-
-    (glob, rt)
-});
-
-fn main() {
-    RT.with(|&(ref glob, ref rt)| unsafe {
         let _ac = JSAutoCompartment::new(rt.cx(), glob.get());
 
-        rooted!(in(rt.cx()) let mut rval = UndefinedValue());
+        JS_InitReflectParse(rt.cx(), glob_handle);
+        JS_FireOnNewGlobalObject(rt.cx(), glob_handle);
 
-        rt.evaluate_script(
-            rust::Handle::from_raw(glob.handle()),
-            "'1 + 2 = ' + (1 + 2)",
-            "nofile.js",
-            1,
-            rval.handle_mut(),
-        ).unwrap();
+        _ac
+    };
 
+    rooted!(in(rt.cx()) let mut rval = UndefinedValue());
+
+    rt.evaluate_script(
+        glob_handle,
+        JAVASCRIPT,
+        "nofile.js",
+        1,
+        rval.handle_mut(),
+    ).unwrap();
+
+    let chars = unsafe {
         let s = rval.get().to_string();
-        let chars = JS_EncodeStringToUTF8(rt.cx(), Handle::from_marked_location(&s));
-        let chars = ::std::ffi::CStr::from_ptr(chars);
+        let s = JS_EncodeStringToUTF8(
+            rt.cx(), Handle::from_marked_location(&s));
+        let s = ::std::ffi::CStr::from_ptr(s);
+        String::from(s.to_string_lossy())
+    };
 
-        println!("result: {}", chars.to_string_lossy());
-    });
+    println!("result: {}", chars);
 }
